ddbc45f81afd287cda78c4ded4369fe9cb4c41ed
Paige Mo <38977897+JiuXia2025@users.noreply.github.com>
2024-04-17 16:45:12 +0800

    Update Start.bat

--- OnekeyInstallWindowsStore/Start.bat
+++ OnekeyInstallWindowsStore/Start.bat
@@ -3,14 +3,17 @@ title Current Directory:%~dp0
 echo *******************************************
 echo         OnekeyInstallWindowsStore
 echo              Github@JiuXia2025
-echo           http://blog.9xia.top
+echo           https://blog.inekoxia.com
 echo *******************************************
 pause
-powershell Add-AppxPackage -Path %~dp0Microsoft.NET.Native.Framework.2.2_2.2.29512.0_x64__8wekyb3d8bbwe.Appx
-powershell Add-AppxPackage -Path %~dp0Microsoft.NET.Native.Runtime.2.2_2.2.28604.0_x64__8wekyb3d8bbwe.Appx
-powershell Add-AppxPackage -Path %~dp0Microsoft.UI.Xaml.2.4_2.42007.9001.0_x64__8wekyb3d8bbwe.Appx
-powershell Add-AppxPackage -Path %~dp0Microsoft.VCLibs.140.00_14.0.30704.0_x64__8wekyb3d8bbwe.Appx
-powershell Add-AppxPackage -Path %~dp0Microsoft.WindowsStore_12107.1001.15.0_neutral___8wekyb3d8bbwe.AppxBundle
+set "folderPath=%~dp0"
+
+for %%i in ("%folderPath%\*.appx") do (
+    echo Installing %%~nxi...
+    powershell Add-AppxPackage -Path "%%i"
+)
+
+echo All Appx packages installed.
 echo done
 pause
 exit
